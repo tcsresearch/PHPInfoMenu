66fbc2bfb0ec2ef159860cfa0fdcf90f5dc2491d
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-03-24 13:07:47 -0500

    Update menu links to use new _Indexes path

--- MainMenu.inc.php
+++ MainMenu.inc.php
@@ -2,12 +2,13 @@
 
 <ul class="menu" role="navigation">
   <li><a class="active" href="#Home">Home</a></li>
-  <li><a href="Index_General.php">General</a></li>
-  <li><a href="Index_Credits.php">Credits</a></li>
-  <li><a href="Index_Configuration.php">Configuration</a></li>
-  <li><a href="Index_Modules.php">Modules</a></li>
-  <li><a href="Index_Environment.php">Environment</a></li>
-  <li><a href="Index_Variables.php">Variables</a></li>
-  <li><a href="Index_License.php">Licence</a></li>
-  <li><a href="Index_All.php">All</a></li>
+  <li><a href="/_Indexes/Index_General.php">General</a></li>
+  <li><a href="/_Indexes/Index_Credits.php">Credits</a></li>
+  <li><a href="/_Indexes/Index_Configuration.php">Configuration</a></li>
+  <li><a href="/_Indexes/Index_Modules.php">Modules</a></li>
+  <li><a href="/_Indexes/Index_Environment.php">Environment</a></li>
+  <li><a href="/_Indexes/Index_Variables.php">Variables</a></li>
+  <li><a href="/_Indexes/Index_License.php">Licence</a></li>
+  <li><a href="/_Indexes/Index_All.php">All</a></li>
 </ul>
+
