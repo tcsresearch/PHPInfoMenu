490cf6750b5cddfd0e957fc956a26c7d58127078
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-03-27 14:43:24 -0500

    Major code refactor - Mar 27 2026
    Bugfixes galore.
    MainMenu Enabled.
    CSS Enabled.
    
    Workable copy with minor CSS glitches on a few pages.

--- MainMenu.inc.php
+++ MainMenu.inc.php
@@ -1,25 +1,13 @@
-<?php // TODO: Convert full file to use PHP code? ?>
-
-
-<link rel="stylesheet" href="/css/style.css">
-
-<?php // Require calling MainMenu.inc.php ?>
-<?php require_once 'MainMenu.inc.php'; ?>
-
-<?php // Consider using relative path ?>
-// <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/menu.php'; ?>
-
-
+<link rel="stylesheet" href="/style.css">
 
 <ul class="menu" role="navigation">
   <li><a class="active" href="#Home">Home</a></li>
-  <li><a href="/_Indexes/Index_General.php">General</a></li>
-  <li><a href="/_Indexes/Index_Credits.php">Credits</a></li>
-  <li><a href="/_Indexes/Index_Configuration.php">Configuration</a></li>
-  <li><a href="/_Indexes/Index_Modules.php">Modules</a></li>
-  <li><a href="/_Indexes/Index_Environment.php">Environment</a></li>
-  <li><a href="/_Indexes/Index_Variables.php">Variables</a></li>
-  <li><a href="/_Indexes/Index_License.php">Licence</a></li>
-  <li><a href="/_Indexes/Index_All.php">All</a></li>
+  <li><a href="Index_General.php">General</a></li>
+  <li><a href="Index_Credits.php">Credits</a></li>
+  <li><a href="Index_Configuration.php">Configuration</a></li>
+  <li><a href="Index_Modules.php">Modules</a></li>
+  <li><a href="Index_Environment.php">Environment</a></li>
+  <li><a href="Index_Variables.php">Variables</a></li>
+  <li><a href="Index_License.php">Licence</a></li>
+  <li><a href="Index_All.php">All</a></li>
 </ul>
-
